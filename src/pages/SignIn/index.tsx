c3404f5cc4f6c0c82aa6bcae74730a331a403d55
Fernando Ferreira Rocha <fernandofr510@gmail.com>
2020-07-31 17:35:47 -0300

    add style page signin

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/mobile';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { KeyboardAvoidingView, ScrollView, Platform, View, TextInput } from 'react-native';
+
 import * as Styled from './styles';
 
 const SignIn: React.FC = () => {
-  return <Styled.Container />;
+  const formRef = useRef<FormHandles>(null); 
+  const passwordInputRef = useRef<TextInput>(null);
+  
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data)
+  }, [])
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={ Platform.OS === 'ios' ? 'padding' : undefined}
+      enabled
+    >
+      <ScrollView 
+        keyboardShouldPersistTaps='handled'
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Styled.Container>
+          <View>
+            <Styled.Title>Faça seu Login</Styled.Title>
+          </View>
+
+          <Form ref={formRef} onSubmit={handleSignIn}>
+            <Input 
+              name='username' 
+              icon='user' 
+              placeholder="usuário" 
+              autoCorrect={false}
+              autoCapitalize="none"
+              returnKeyType='next'
+              onSubmitEditing={() => {
+                passwordInputRef.current?.focus()
+              }}
+            />
+            <Input 
+              ref={passwordInputRef}
+              name='password' 
+              icon='lock' 
+              placeholder='senha' 
+              secureTextEntry
+              returnKeyType="send"
+              onSubmitEditing={() => formRef.current?.submitForm()}
+            />
+          </Form>
+          <Button onPress={() => formRef.current?.submitForm()}>
+            Entrar
+          </Button>
+        </Styled.Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
 
 export default SignIn;
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 
 export const Container = styled.View`
-  
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 120 : 40}px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  margin: 64px 0 24px;
 `;
